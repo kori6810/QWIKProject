c171557cfc4d14f84dc4b192869701229ce8e1e8
Jamoldin <jamoldin200310@gmail.com>
2023-05-20 19:34:29 +0500

    Final Commit 4

--- app/src/main/java/com/example/mpproject/MainActivity.java
+++ app/src/main/java/com/example/mpproject/MainActivity.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mpproject.databinding.ActivityMainBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private BottomSheetDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        dialog=new BottomSheetDialog(this,R.style.BottomSheetStyle);
+        showBottomSheet();
 
         binding.registerBtn.setOnClickListener(view->{
-            startActivity(new Intent(this, RegisterActivity.class));
+            dialog.show();
         });
 
         binding.loginBtn.setOnClickListener(view->{
@@ -26,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+    }
+
+    private  void showBottomSheet(){
+        View dialogView=getLayoutInflater().inflate(R.layout.bottom_sheet,null,false);
+        TextView loginBtn=dialogView.findViewById(R.id.login_btn);
+        String text="<font color=#000000>Already have an account? </font><font color=#3254CB>LOG IN</font>";
+        loginBtn.setText(Html.fromHtml(text));
+        loginBtn.setOnClickListener(view->{
+            startActivity(new Intent(this,LoginActivity.class));
+        });
+        dialog.setContentView(dialogView);
     }
 }
